603925a63d0f0cf3d867d84b6db9241b24ec19ef
MikhailBudyakov <mikhail.budyakov@immelman.ru>
2023-02-13 01:23:54 +0300

    Save avatar

--- app/Http/Controllers/Api/User/UpdateProfile.php
+++ app/Http/Controllers/Api/User/UpdateProfile.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Api\User;
 
+use App\Models\Attachment;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UpdateProfile
 {
@@ -16,12 +19,25 @@ class UpdateProfile
          */
         $user = Auth::user();
 
-        $user->update([
-            'name' => $request->name,
-            'description' => $request->description,
-            'email' => $request->email,
-            'login' => $request->login,
-        ]);
+        DB::transaction(function () use ($user, $request) {
+            $user->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'email' => $request->email,
+                'login' => $request->login,
+            ]);
+
+            if ($file = $request->file('avatar')) {
+                $attachment = new Attachment(['link' => 'temp']);
+                $user->avatar()->save($attachment);
+                $fileName = Str::random(32) . '_' . $attachment->id . '.' . $file->getClientOriginalExtension();
+                $file->storeAs('attachments', $fileName);
+
+                $attachment->link = $fileName;
+                $attachment->save();
+            }
+        });
+
 
         return response()->json();
     }
